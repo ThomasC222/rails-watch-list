f2c4cde70bf8a006a3982f09085cded02f6667be
Thomas Chabanne <tchabanne@gmail.com>
2021-11-19 11:11:56 +0100

    on ne sait jamais

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,21 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(params[:id])
+    @movie.save
+
+    redirect_to movies_path
+  end
+
+end
